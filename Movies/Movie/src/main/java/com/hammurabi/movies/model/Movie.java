d70a5c0dee020ab9eda03d518aad1fe8e698c37e
Sihle <lodrick.mpanze0@gmail.com>
2019-10-19 22:07:27 +0200

    Packages creation

--- Movies/Movie/src/main/java/com/hammurabi/movies/model/Movie.java
+++ Movies/Movie/src/main/java/com/hammurabi/movies/model/Movie.java
@@ -2,12 +2,19 @@ package com.hammurabi.movies.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Movie implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2979301804360417919L;
 	
+	@Id
+	@GeneratedValue
 	private long mid;
 	private String mName;
 	private String mDescription;
