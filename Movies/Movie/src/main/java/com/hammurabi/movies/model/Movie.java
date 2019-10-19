4a9d351dd3fe3eb3410fc391769bcbb095d27453
Sihle <lodrick.mpanze0@gmail.com>
2019-10-19 21:42:59 +0200

    Movie model generate

---
+++ Movies/Movie/src/main/java/com/hammurabi/movies/model/Movie.java
@@ -0,0 +1,41 @@
+package com.hammurabi.movies.model;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2979301804360417919L;
+	
+	private long mid;
+	private String mName;
+	private String mDescription;
+	private String genre;
+	
+	public long getMid() {
+		return mid;
+	}
+	public void setMid(long mid) {
+		this.mid = mid;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public String getmDescription() {
+		return mDescription;
+	}
+	public void setmDescription(String mDescription) {
+		this.mDescription = mDescription;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+}
